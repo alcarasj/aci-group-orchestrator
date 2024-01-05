171deb629d53e1b9d3735207cb74f0e4a2f63857
Jerico Alcaras <jalcaras@microsoft.com>
2024-01-05 16:10:22 +0000

    Use system-assigned managed identity of VM.

--- Program.cs
+++ Program.cs
@@ -23,54 +23,29 @@ public static class Program
 
         if (shouldOrchestrateOnTheCloud)
         {
-            // This block starts a confidential container group via ARM using the Azure .NET SDK.
-            // Authentication is done via env vars and the "az login" that is executed in the same terminal context.
-            var credentials = new DefaultAzureCredential();
+            var credentials = new ManagedIdentityCredential();
             var armClient = new ArmClient(credentials);
             SubscriptionCollection subscriptions = armClient.GetSubscriptions();
             SubscriptionResource subscription = subscriptions.Get(targetSubscriptionId);
             ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
             ResourceGroupResource resourceGroup = resourceGroups.Get(targetResourceGroupName);
             ContainerGroupCollection collection = resourceGroup.GetContainerGroups();
-#pragma warning disable SA1118 // Parameter should not span multiple lines
-#pragma warning disable SA1117 // Parameters should be on same line or separate lines
             ContainerGroupData data = new ContainerGroupData(new AzureLocation("westeurope"), new ContainerInstanceContainer[]
                 {
-new ContainerInstanceContainer("accdemo", "mcr.microsoft.com/azuredocs/aci-helloworld", new ContainerResourceRequirements(new ContainerResourceRequestsContent(1.5, 1)))
-{
-Command =
-{
-}, 
-Ports =
-{
-new ContainerPort(8000)
-},
-EnvironmentVariables =
-{
-},
-SecurityContext = new ContainerSecurityContextDefinition()
-{
-IsPrivileged = false,
-},
-}
-                }, ContainerInstanceOperatingSystemType.Linux)
+                    new ContainerInstanceContainer("accdemo", "mcr.microsoft.com/azuredocs/aci-helloworld", new ContainerResourceRequirements(new ContainerResourceRequestsContent(1.5, 1)))
+                    {
+                        Command={},
+                        Ports ={new ContainerPort(8000)},
+                        EnvironmentVariables = {},
+                        SecurityContext = new ContainerSecurityContextDefinition(){IsPrivileged = false}}
+                    }, 
+                    ContainerInstanceOperatingSystemType.Linux)
             {
-                ImageRegistryCredentials =
-{
-},
-                IPAddress = new ContainerGroupIPAddress(
-                    new ContainerGroupPort[]
-                {
-new ContainerGroupPort(8000)
-{
-Protocol = ContainerGroupNetworkProtocol.Tcp,
-}
-                }, ContainerGroupIPAddressType.Public),
+                ImageRegistryCredentials ={},
+                IPAddress = new ContainerGroupIPAddress(new ContainerGroupPort[]{new ContainerGroupPort(8000){Protocol = ContainerGroupNetworkProtocol.Tcp}}, ContainerGroupIPAddressType.Public),
                 Sku = ContainerGroupSku.Confidential,
                 ConfidentialComputeCcePolicy = "eyJhbGxvd19hbGwiOiB0cnVlLCAiY29udGFpbmVycyI6IHsibGVuZ3RoIjogMCwgImVsZW1lbnRzIjogbnVsbH19",
             };
-#pragma warning restore SA1117 // Parameters should be on same line or separate lines
-#pragma warning restore SA1118 // Parameter should not span multiple lines
             ArmOperation<ContainerGroupResource> lro = collection.CreateOrUpdate(WaitUntil.Completed, containerGroupName, data);
             ContainerGroupResource result = lro.Value;
             ContainerGroupData resourceData = result.Data;
