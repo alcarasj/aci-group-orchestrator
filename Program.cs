5df0db230cc4b15cc07ab698a137216f4ef375b0
Jerico Alcaras <jalcaras@microsoft.com>
2024-01-08 13:25:14 +0000

    Improvements and delete.

--- Program.cs
+++ Program.cs
@@ -10,49 +10,73 @@ public static class Program
 {
     private static void Main()
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
-        StartContainerGroup();
+        var targetSubscriptionId = GetEnvVar("AZURE_SUBSCRIPTION_ID");
+        var targetResourceGroupName = Environment.GetEnvironmentVariable("TARGET_RESOURCE_GROUP_NAME");
+        var containerGroupName = Environment.GetEnvironmentVariable("CONTAINER_GROUP_NAME"); ;
+        var containerGroupId = StartContainerGroup(targetSubscriptionId, targetResourceGroupName, containerGroupName);
+        DeleteContainerGroup(targetSubscriptionId, targetResourceGroupName, containerGroupName);
     }
 
-    private static void StartContainerGroup()
+    private static string StartContainerGroup(string targetSubscriptionId, string targetResourceGroupName, string containerGroupName)
     {
-        var targetSubscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
-        var targetResourceGroupName = Environment.GetEnvironmentVariable("TARGET_RESOURCE_GROUP_NAME");
-        var containerGroupName = "hello-world-container-group"; // Environment.GetEnvironmentVariable("CONTAINER_GROUP_NAME");
-        var shouldOrchestrateOnTheCloud = !string.IsNullOrEmpty(targetSubscriptionId) && !string.IsNullOrEmpty(targetResourceGroupName);
-
-        if (shouldOrchestrateOnTheCloud)
-        {
-            var credentials = new ManagedIdentityCredential();
-            var armClient = new ArmClient(credentials);
-            SubscriptionCollection subscriptions = armClient.GetSubscriptions();
-            SubscriptionResource subscription = subscriptions.Get(targetSubscriptionId);
-            ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
-            ResourceGroupResource resourceGroup = resourceGroups.Get(targetResourceGroupName);
-            ContainerGroupCollection collection = resourceGroup.GetContainerGroups();
-            ContainerGroupData data = new ContainerGroupData(new AzureLocation("westeurope"), new ContainerInstanceContainer[]
-                {
+        Console.WriteLine($"\n\nCreating container group with name {containerGroupName} in resource group {targetResourceGroupName} for subscription {targetSubscriptionId}...");
+        var credentials = new ManagedIdentityCredential();
+        var armClient = new ArmClient(credentials);
+        SubscriptionCollection subscriptions = armClient.GetSubscriptions();
+        SubscriptionResource subscription = subscriptions.Get(targetSubscriptionId);
+        ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
+        ResourceGroupResource resourceGroup = resourceGroups.Get(targetResourceGroupName);
+        ContainerGroupCollection collection = resourceGroup.GetContainerGroups();
+        ContainerGroupData data = new ContainerGroupData(new AzureLocation("westeurope"), new ContainerInstanceContainer[]
+            {
                     new ContainerInstanceContainer("accdemo", "mcr.microsoft.com/azuredocs/aci-helloworld", new ContainerResourceRequirements(new ContainerResourceRequestsContent(1.5, 1)))
                     {
                         Command={},
                         Ports ={new ContainerPort(8000)},
                         EnvironmentVariables = {},
-                        SecurityContext = new ContainerSecurityContextDefinition(){IsPrivileged = false}}
-                    }, 
-                    ContainerInstanceOperatingSystemType.Linux)
-            {
-                ImageRegistryCredentials ={},
-                IPAddress = new ContainerGroupIPAddress(new ContainerGroupPort[]{new ContainerGroupPort(8000){Protocol = ContainerGroupNetworkProtocol.Tcp}}, ContainerGroupIPAddressType.Public),
-                Sku = ContainerGroupSku.Confidential,
-                ConfidentialComputeCcePolicy = "eyJhbGxvd19hbGwiOiB0cnVlLCAiY29udGFpbmVycyI6IHsibGVuZ3RoIjogMCwgImVsZW1lbnRzIjogbnVsbH19",
-            };
-            ArmOperation<ContainerGroupResource> lro = collection.CreateOrUpdate(WaitUntil.Completed, containerGroupName, data);
-            ContainerGroupResource result = lro.Value;
-            ContainerGroupData resourceData = result.Data;
-            Console.WriteLine($"Successfully created container group with ID {resourceData.Id}");
-        }
+                        SecurityContext = new ContainerSecurityContextDefinition(){IsPrivileged = false}
+                    }
+                },
+                ContainerInstanceOperatingSystemType.Linux)
+        {
+            ImageRegistryCredentials = { },
+            IPAddress = new ContainerGroupIPAddress(new ContainerGroupPort[] { new ContainerGroupPort(8000) { Protocol = ContainerGroupNetworkProtocol.Tcp } }, ContainerGroupIPAddressType.Public),
+            Sku = ContainerGroupSku.Confidential,
+            ConfidentialComputeCcePolicy = "eyJhbGxvd19hbGwiOiB0cnVlLCAiY29udGFpbmVycyI6IHsibGVuZ3RoIjogMCwgImVsZW1lbnRzIjogbnVsbH19",
+        };
+        ArmOperation<ContainerGroupResource> lro = collection.CreateOrUpdate(WaitUntil.Completed, containerGroupName, data);
+        ContainerGroupResource result = lro.Value;
+        ContainerGroupData resourceData = result.Data;
+        Console.WriteLine($"\n\nSuccessfully created container group {containerGroupName} with ID {resourceData.Id} in in resource group {targetResourceGroupName} for subscription {targetSubscriptionId}");
+        return resourceData.Id;
+    }
 
-        // TO-DO Establish and verify connections to the required containers.
+    private static void DeleteContainerGroup(string targetSubscriptionId, string targetResourceGroupName, string containerGroupName)
+    {
+        Console.WriteLine($"\n\nDeleting container group with name {containerGroupName} in resource group {targetResourceGroupName} for subscription {targetSubscriptionId}...");
+        var credentials = new ManagedIdentityCredential();
+        var armClient = new ArmClient(credentials);
+        ResourceIdentifier containerGroupResourceId = ContainerGroupResource.CreateResourceIdentifier(targetSubscriptionId, targetResourceGroupName, containerGroupName);
+        ContainerGroupResource containerGroup = armClient.GetContainerGroupResource(containerGroupResourceId);
+        ArmOperation<ContainerGroupResource> operation = containerGroup.Delete(WaitUntil.Completed);
+        ContainerGroupResource result = operation.Value;
+        ContainerGroupData resourceData = result.Data;
+
+        Console.WriteLine($"\n\nSuccessfully deleted container group {containerGroupName} (ID {resourceData.Id}) in resource group {targetResourceGroupName} for subscription {targetSubscriptionId}");
+    }
+
+    private static void DeleteAllContainerGroups(string targetSubscriptionId, string targetResourceGroupName)
+    {
+
+    }
+
+    private static string GetEnvVar(string envVarName)
+    {
+        var value = Environment.GetEnvironmentVariable(envVarName);
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new Exception($"{envVarName} is not set!");
+        }
+        return value;
     }
 }
