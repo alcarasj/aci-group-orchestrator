3dc6c0a5a7516eea0cb2a20b03ca4a6d7ee5f130
Jerico Alcaras <jalcaras@microsoft.com>
2024-10-10 21:42:50 +0100

    Amend RG name.

--- init.ps1
+++ init.ps1
@@ -1,5 +1,5 @@
 $env:AZURE_SUBSCRIPTION_ID = "e0f91dc0-102c-41ae-a3b3-d256a2ee118d"
-$env:TARGET_RESOURCE_GROUP_NAME = "jericos-stuff-rg"
+$env:TARGET_RESOURCE_GROUP_NAME = "jericos-stuff-uaen"
 $env:CONTAINER_GROUP_NAME = "hello-world-cg"
 $env:TARGET_SUBNET_RESOURCE_ID = "/subscriptions/e0f91dc0-102c-41ae-a3b3-d256a2ee118d/resourceGroups/jericos-stuff-uaen/providers/Microsoft.Network/virtualNetworks/jericos-vnet/subnets/aci-cg-subnet"
 $env:TARGET_SUBNET_NAME ="aci-cg-subnet"
