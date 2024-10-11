f628521ba0502935bada87bc6f698f4461189b67
Jerico Alcaras <jalcaras@microsoft.com>
2024-10-11 16:24:12 +0100

    Add DNS updater container.

--- DnsUpdater/run.sh
+++ DnsUpdater/run.sh
@@ -1,2 +1,3 @@
-az login
+az login --identity --username "/subscriptions/e0f91dc0-102c-41ae-a3b3-d256a2ee118d/resourceGroups/jericos-stuff-uaen/providers/Microsoft.ManagedIdentity/userAssignedIdentities/jericos-uami"
+az network private-dns record-set a add-record -g jericos-stuff-uaen -z jericos.stuff -n some-record-set -a "1.2.3.4"
 dotnet DnsUpdater.dll
